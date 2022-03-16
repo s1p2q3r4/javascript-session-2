5cf78ad4c1f9f023eae9696340e2f88d3201354b
s1p2q3r4 <99183896+s1p2q3r4@users.noreply.github.com>
2022-03-16 20:49:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var str="Skill Safari"
+console .log(str)
+ console.log(str.length)
+ 
+ console.log(str.toUpperCase(str))
+ console.log(str.indexOf("a"))
+  console.log("Captain America:This isn't going to change what happened\n Iron Man:I don't care,he killed my mom")
+
+  var weight=60
+  var height=1.5
+  const ratio=((weight)/(height*height))
+  console.log(ratio)
